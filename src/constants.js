a43b88197287eac1bc7d277aec1b86c4c592829e
Nicolas Arias <elbunuelo@gmail.com>
2018-04-23 12:34:49 -0400

    Fix url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 //const BASE_URL = 'http://localhost:3333';
-const BASE_URL = 'https://beer-recipe-manager.herokuapp.com';
+const BASE_URL = 'https://beer-recipe-manager-backend.herokuapp.com';
 export default {
     backendUrl: BASE_URL,
     authUrl: BASE_URL + "/auth",
